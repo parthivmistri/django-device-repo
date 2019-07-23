52b701619366b9ccf3c93a901d552545cd03da63
Parthiv <pmistri@codal.com>
2019-07-23 16:38:28 +0530

    removed default pagination

--- device_repo/settings.py
+++ device_repo/settings.py
@@ -62,7 +62,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.pagination.LimitOffsetPagination',
     )
 }
 
